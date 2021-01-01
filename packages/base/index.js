9260143866f86900f9d9ee627b1f7de559626bb8
Darin Cassler <darin@cassler.net>
2021-01-01 12:24:36 -0600

    add custom rules

--- packages/base/index.js
+++ packages/base/index.js
@@ -22,11 +22,32 @@ module.exports = {
   plugins: ['prettier', 'import'],
 
   rules: {
-    'prettier/prettier': 'error',
+    "prettier/prettier": [
+      "error",
+      {
+        "trailingComma": "es5",
+        "singleQuote": true,
+        "printWidth": 80
+      }
+    ],
+    "arrow-body-style": [
+      2,
+      "as-needed"
+    ],
 
     'import/no-extraneous-dependencies': ['error', {
       devDependencies: false,
     }],
+
+    "jsx-a11y/href-no-hash": "off",
+    "jsx-a11y/anchor-is-valid": [
+      "warn",
+      {
+        "aspects": [
+          "invalidHref"
+        ]
+      }
+    ],
 
     'one-var': 0,
     'one-var-declaration-per-line': 0,
@@ -34,7 +55,66 @@ module.exports = {
     strict: 0,
     'no-use-before-define': [2, { functions: false }],
     'no-underscore-dangle': 0,
+    "no-param-reassign": [
+      2,
+      {
+        "props": false
+      }
+    ],
     'no-plusplus': 0,
+    "no-return-assign": [
+      "error",
+      "except-parens"
+    ],
+    "no-restricted-syntax": [
+      2,
+      "ForInStatement",
+      "LabeledStatement",
+      "WithStatement"
+    ],
+    "no-shadow": [
+      2,
+      {
+        "hoist": "all",
+        "allow": [
+          "resolve",
+          "reject",
+          "done",
+          "next",
+          "err",
+          "error"
+        ]
+      }
+    ],
+    "no-unused-expressions": [
+      2,
+      {
+        "allowTaggedTemplates": true
+      }
+    ],
+    "no-unused-vars": [
+      1,
+      {
+        "ignoreSiblings": true,
+        "argsIgnorePattern": "res|next|^err"
+      }
+    ],
+    "prefer-const": [
+      "error",
+      {
+        "destructuring": "all"
+      }
+    ],
+
+    "quotes": [
+      2,
+      "single",
+      {
+        "avoidEscape": true,
+        "allowTemplateLiterals": true
+      }
+    ],
+    "radix": 0,
   },
 
   overrides: [
