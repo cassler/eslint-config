5cbfa41c0c8cacf914270e419f51e400319559bd
Darin Cassler <darin@cassler.net>
2021-01-01 14:26:29 -0600

    hotfix

--- packages/base/index.js
+++ packages/base/index.js
@@ -44,7 +44,7 @@ module.exports = {
     }],
 
     "jsx-a11y/href-no-hash": "off",
-    "jsx-a11y/anchor-is-valid": 2,
+    // "jsx-a11y/anchor-is-valid": 2,
     'one-var': 0,
     'one-var-declaration-per-line': 0,
     'prefer-arrow-callback': 0,
